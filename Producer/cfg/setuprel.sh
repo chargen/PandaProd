41358d3db5d64e507372c68c00f0c6ba2cc71bce
Sid Narayanan <sidn@mit.edu>
2017-03-20 08:05:44 -0400

    adding met 80x v2 recipe checkout

--- Producer/cfg/setuprel.sh
+++ Producer/cfg/setuprel.sh
@@ -8,3 +8,5 @@ $INSTALL cms-met METRecipe_8020 RecoMET/METFilters PhysicsTools/PatAlgos Physics
 # TEMPORARY UNTIL cms-met pulls this branch
 $INSTALL yiiyama puppiphoton_filteredpf CommonTools/PileupAlgos
 $INSTALL ikrav egm_id_80X_v3_photons PhysicsTools/SelectorUtils RecoEgamma/PhotonIdentification RecoEgamma/EgammaIsolationAlgos
+$INSTALL cms-met METRecipe_80X_part2 CommonTools/PileupAlgos DataFormats/PatCandidates PhysicsTools/PatAlgos PhysicsTools/PatUtils RecoMET/METAlgorithms RecoMET/METFilters # RecoMET/METProducers  # should uncomment if rerun MC
+
